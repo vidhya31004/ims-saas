b2858d56f33772bc3e4c6b421e9aae7e436e3354
Vidhya Shree Mohana Navaneethan <vidhya@Vidhyas-MacBook-Air.local>
2026-03-25 15:49:17 +0530

    frontend connected to backend

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-# Use SQLite instead of PostgreSQL for now
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
